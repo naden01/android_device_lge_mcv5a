0160305737ef07df3f3f14a8f212dc74e747da40
nazephyrus <126683488+naden01@users.noreply.github.com>
2025-07-21 06:07:59 -0700

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,12 +1,12 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),mcv5a)
-
+ifeq ($(TARGET_DEVICE),k50sv1_64)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
-
-# Hack some props to allow stock ROM flashing
-BOARD_RECOVERY_IMAGE_PREPARE := \
-    sed -i 's/ro.bootimage.build.date.utc=.*/ro.bootimage.build.date.utc=0/' $(TARGET_RECOVERY_ROOT_OUT)/prop.default; \
-    sed -i 's/ro.build.date.utc=.*/ro.build.date.utc=0/' $(TARGET_RECOVERY_ROOT_OUT)/prop.default;
-
 endif
